f03219cea75d597c3ce513e8cf02e73497bc3569
ShatalovM <shatalovmichael@gmail.com>
2017-08-21 10:32:46 +0300

    1.1
    UPD: Places Activity

--- app/src/main/java/kareliaguide/vsquad/com/karelia/PlaceActivity.java
+++ app/src/main/java/kareliaguide/vsquad/com/karelia/PlaceActivity.java
@@ -15,26 +15,88 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlaceActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     public TextView place_description;
+    public FloatingActionButton icon_or_next;
+    public ImageView background_photo;
 
-    public String[] Places_titles = {
+    // БД для всех мест
+    public String[][] Places_base = {{
             "",
             "",
             ""
+    },{
+            "",
+            "",
+            ""
+    }};
+
+    // БД для категорий
+    public String[][] Category_trees = {{
+            // Titles
+            ""
+    },{
+            // Descriptions
+            ""
+    }};
+
+    public String[][] Category_culture = {{
+            // Titles
+            ""
+    },{
+            // Descriptions
+            ""
+    }};
+
+    public String[][] Category_active = {{
+            // Titles
+            ""
+    },{
+            // Descriptions
+            ""
+    }};
+
+    public String[][] Category_food = {{
+            // Titles
+            ""
+    },{
+            // Descriptions
+            ""
+    }};
+
+    public String[][] Category_popular = {{
+            // Titles
+            ""
+    },{ // Descriptions
+            ""
+    }};
+
+    // Nearby - ???
+
+
+    // БД долготы и широты для всех мест; Потом изменить по номерам
+    public double[] Places_latitude = {
+            1.0,
+            1.0,
+            1.0
     };
 
-    public String[] Places_description = {
-            "",
-            "",
-            ""
+    public double[] Places_longitude = {
+            1.0,
+            1.0,
+            1.0
     };
 
     public int place_num;
+    public int shown_place = 0;
+    public double lat;
+    public double lon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +115,8 @@ public class PlaceActivity extends AppCompatActivity
         });
 
         place_description = (TextView) findViewById(R.id.place_description);
+        icon_or_next = (FloatingActionButton) findViewById(R.id.icon_or_next);
+        background_photo = (ImageView) findViewById(R.id.main_backdrop);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,16 +131,82 @@ public class PlaceActivity extends AppCompatActivity
 
         if (getData != null) {
             place_num = getData.getInt(""); // Получаем номер места, необходимого для подгрузки
+            lat = getData.getDouble(""); // Получаем широту места
+            lon = getData.getDouble(""); // Получаем долготу места
         }
 
         /*
         [0;n]: номер места
         {-1}: место не указано
         [-7;-2]: номер категории для отображения
+
+        {-2}: активный отдых
+        {-3}: культурный отдых
+        {-4}: природа
+        {-5}: еда, гурман
         */
 
-        setTitle("");
-        place_description.setText("");
+        switch (place_num) {
+            // Определение единичных мест
+            case 0:
+                setTitle("");
+                place_description.setText("");
+            case 1:
+                setTitle("");
+                place_description.setText("");
+            case 2:
+                setTitle("");
+                place_description.setText("");
+            case 3:
+                setTitle("");
+                place_description.setText("");
+            case 4:
+                setTitle("");
+                place_description.setText("");
+            case 5:
+                setTitle("");
+                place_description.setText("");
+
+            // Определение по категориям
+            case -2:
+                icon_or_next.setImageResource(R.drawable.ic_menu_send);
+                setTitle(Category_active[shown_place%Category_active.length][shown_place+1%Category_active.length]);
+                place_description.setText(Category_active[shown_place%Category_active.length][shown_place+1%Category_active.length]);
+                background_photo.setImageResource(R.drawable.karelia);
+            case -3:
+                icon_or_next.setImageResource(R.drawable.ic_menu_send);
+                setTitle("");
+                place_description.setText("");
+            case -4:
+                icon_or_next.setImageResource(R.drawable.ic_menu_send);
+                setTitle("");
+                place_description.setText("");
+            case -5:
+                icon_or_next.setImageResource(R.drawable.ic_menu_send);
+                setTitle("");
+                place_description.setText("");
+            case -6:
+                icon_or_next.setImageResource(R.drawable.ic_menu_send);
+                setTitle("");
+                place_description.setText("");
+            case -7:
+                icon_or_next.setImageResource(R.drawable.ic_menu_send);
+                setTitle("");
+                place_description.setText("");
+
+            // Место не указано
+            case -1:
+                // обработка
+        }
+    }
+
+    public void ic_next (View view){
+        if (place_num <= -2 && place_num >= -7){
+            shown_place++;
+            setTitle(Category_active[shown_place%Category_active.length][shown_place+1%Category_active.length]);
+            place_description.setText(Category_active[shown_place%Category_active.length][shown_place+1%Category_active.length]);
+            background_photo.setImageResource(R.drawable.karelia);
+        }
     }
 
     @Override
---
+++ app/src/main/java/kareliaguide/vsquad/com/karelia/PlacesForMapCorrectActivity.java
@@ -0,0 +1,13 @@
+package kareliaguide.vsquad.com.karelia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlacesForMapCorrectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_for_map_correct);
+    }
+}
